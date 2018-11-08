ba3a017655739366a1bad6f275a92e75630fd0db
azarrias <adolfo.zarrias@gmail.com>
2018-11-08 19:55:37 +0100

    Add OkHttp library and create AsyncTask subclass to download webpage code

--- app/src/main/java/es/pue/android/asynchronoustasks/MainActivity.java
+++ app/src/main/java/es/pue/android/asynchronoustasks/MainActivity.java
@@ -1,13 +1,56 @@
 package es.pue.android.asynchronoustasks;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txData = (TextView)findViewById(R.id.tvCode);
+    }
+
+    public void loadCode(View view) {
+        new DownloadWebpageCode().execute("https://developer.android.com/");
+    }
+
+    private class DownloadWebpageCode extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPostExecute(String s) {
+            txData.setText(s);
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(strings[0])
+                    .build();
+
+            try {
+                Response response = client.newCall(request).execute();
+                if (response.isSuccessful()) {
+                    return response.body().string();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return "No code";
+        }
     }
 }
